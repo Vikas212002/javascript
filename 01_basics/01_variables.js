5b11e76d2dbfb3d5fb0f013792ffe9da4817790f
Vikas212002 <80193100+Vikas212002@users.noreply.github.com>
2024-06-19 06:40:40 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId ="12233";
+let accountEmail = "vk@gmail.com";
+var accountPassword = "12344";
+accountCity = "jaipur";
+
+// console.log(typeOf(accountId));
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/* prefer not to use var because of block and function scope */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treats all JS code to newer version
+
+//code readability should be high
+console
+
+//primitive datatypes
+
+//number
+//string
+//bigInt
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//non primitive
+
+//objects
+
+console.log(typeof "vikas");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
